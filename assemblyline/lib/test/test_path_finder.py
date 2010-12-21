1bf946c107dd67510859377b562466dbc4e52580
matthew.iyer <matthew.iyer@9703541f-4702-6fd7-b4dd-98584dff58c8>
2010-12-21 22:13:36 +0000

    initial foray into predicting expressed versus background transcripts

--- assemblyline/lib/test/test_path_finder.py
+++ assemblyline/lib/test/test_path_finder.py
@@ -13,28 +13,38 @@ from transcript_parser import parse_gtf
 from path_finder import get_isoforms
 from cNode import Node
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
 class Test(unittest.TestCase):
 
     def testFindPaths1(self):
         gtf_file = os.path.join(os.path.dirname(__file__), "findpaths1.gtf")
         for locus_transcripts in parse_gtf(open(gtf_file)):
             isoform_graph = IsoformGraph.from_transcripts(locus_transcripts)
-
             tx_id = 0
             for gene_id, tss_id, score, path in get_isoforms(isoform_graph.G, locus_transcripts,
                                                              fraction_major_path=0.000001):
                 print gene_id, tss_id, tx_id, score, path
                 tx_id += 1
-            return
-        
-            id_score_map = get_transcript_score_map(locus_transcripts)
-            H = transform_graph(isoform_graph.G, id_score_map)            
-            start_nodes = [n for (n,d) in H.in_degree_iter()
-                           if (d == 0)]       
-            for start_node in start_nodes:
-                for p in dfs_paths(H, start_node):
-                    print 'final path', str(p)
 
+#    def testFindPaths2(self):
+#        gtf_file = os.path.join(os.path.dirname(__file__), "findpaths2.gtf")
+#        for locus_transcripts in parse_gtf(open(gtf_file)):
+#            isoform_graph = IsoformGraph.from_transcripts(locus_transcripts)
+#            isoform_graph.collapse(overhang_threshold=15)
+#
+##            nx.spring_layout(isoform_graph.G)
+##            nx.draw(isoform_graph.G)
+##            plt.show()
+#
+#            tx_id = 0
+#            for gene_id, tss_id, score, path in get_isoforms(isoform_graph.G, locus_transcripts,
+#                                                             fraction_major_path=0.000001):
+#                print gene_id, tss_id, tx_id, score, path
+#                tx_id += 1
+#                
             #nx.write_dot(isoform_graph.G, "merge_path7.dot")
             #nx.spectral_layout(isoform_graph.G)
             #nx.spring_layout(isoform_graph.G)
--- assemblyline/setup.py
+++ assemblyline/setup.py
@@ -7,8 +7,25 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("lib.cNode", ["lib/cNode.pyx"])]
+# ---- Extension Modules ----------------------------------------------------
+def get_extension_modules():
+    extensions = []
+    # Graph node
+    extensions.append( Extension( "lib.cNode", ["lib/cNode.pyx"] ) )
+    # Interval clustering                
+    extensions.append( Extension( "lib.bx.cluster", [ "lib/bx/cluster.pyx", "lib/bx/intervalcluster.c"], 
+                                  include_dirs=["lib/bx"]) )
+    # Interval intersection
+    extensions.append( Extension( "lib.bx.intersection", [ "lib/bx/intersection.pyx" ] ) )
+    return extensions
 
-setup(name = 'assemblyline',
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = ext_modules)
+def main():
+    setup(name = "assemblyline",
+          ext_modules = get_extension_modules(),
+          author = "Matthew Iyer",
+          author_email = "mkiyer@umich.edu",
+          description = "Transcriptome assembly clustering tool",
+          url = "http://assemblyline.googlecode.com",
+          cmdclass= {'build_ext': build_ext})
+
+if __name__ == '__main__': main()
