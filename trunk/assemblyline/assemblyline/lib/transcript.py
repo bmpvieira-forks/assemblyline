39fe7006d9ff455e1cacdee3204d8578e61c347b
matthew.iyer <matthew.iyer@9703541f-4702-6fd7-b4dd-98584dff58c8>
2011-11-30 22:00:49 +0000

    new classify transcripts module

---
+++ trunk/assemblyline/assemblyline/lib/transcript.py
@@ -0,0 +1,116 @@
+'''
+Created on Nov 30, 2011
+
+@author: mkiyer
+'''
+# strand constants
+POS_STRAND = 0
+NEG_STRAND = 1
+NO_STRAND = 2
+
+# strand format conversion
+_strand_int_to_str = ["+", "-", "."]
+_strand_str_to_int = {"+": POS_STRAND,
+                      "-": NEG_STRAND,
+                      ".": NO_STRAND}
+def strand_str_to_int(strand):
+    return _strand_str_to_int[strand]
+def strand_int_to_str(strand):
+    return _strand_int_to_str[strand]
+
+def interval_overlap(a, b):
+    """return True if two intervals overlap, False otherwise"""
+    return (a.start < b.end) and (b.start < a.end)
+
+class Exon(object):
+    __slots__ = ('start', 'end')
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    def __str__(self):
+        return ("%s-%s" % (self.start, self.end))
+    def __repr__(self):
+        return ("<%s(start=%d end=%d)>" %
+                (self.__class__.__name__, self.start, self.end))
+    def __eq__(self, other):
+        return (self.start == other.start) and (self.end == other.end)
+    def __ne__(self, other):
+        return (self.start != other.start) or (self.end != other.end)
+    def __lt__(self, other):
+        return self.start < other.start
+    def __le__(self, other):        
+        return self.start <= other.start
+    def __gr__(self, other):
+        return self.start > other.start
+    def __ge__(self, other):
+        return self.start >= other.start        
+    def __hash__(self):
+        return (self.start << 16) | (self.end)
+    def is_overlapping(self, other):
+        return interval_overlap(self, other)
+    
+class Transcript(object):
+    __slots__ = ('chrom', 'start', 'end', 'strand', 'id', 'library', 
+                 'sample', 'cohort', 'fpkm', 'cov', 'frac', 'exons', 
+                 'attrs')
+
+    def __init__(self):
+        self.chrom = None
+        self.start = -1
+        self.end = -1
+        self.strand = NO_STRAND
+        self.id = None
+        self.library = None
+        self.sample = None
+        self.cohort = None
+        self.fpkm = 0.0
+        self.cov = 0.0
+        self.frac = 0.0
+        self.exons = None
+        self.attrs = None
+
+    def __str__(self):
+        return ("<%s(chrom='%s', start='%d', end='%d', strand='%s', "
+                "id='%s', library='%s', cohort='%s', sample='%s', fpkm='%s', " 
+                "cov='%s', frac='%s', length='%s', exons='%s'" % 
+                (self.__class__.__name__, self.chrom, self.start, self.end, 
+                 strand_int_to_str(self.strand), self.id, self.cohort, 
+                 self.sample, self.fpkm, self.cov, self.frac, self.length, self.exons))
+
+    @property
+    def tx_start(self):
+        return self.exons[0].start
+    @property
+    def tx_end(self):
+        return self.exons[-1].end
+    @property
+    def length(self):
+        return sum((e.end - e.start) for e in self.exons)
+    @property
+    def mass(self):
+        return self.fpkm * float(self.length) * 1e-3
+    @property
+    def introns(self):
+        return zip([e[1] for e in self.exons[:-1]],
+                   [e[0] for e in self.exons[1:]])
+
+    def to_bed12(self):
+        block_sizes = []
+        block_starts = []
+        for e0, e1 in self.exons:
+            block_starts.append(e0 - self.tx_start)
+            block_sizes.append(e1 - e0)        
+        # write
+        s = '\t'.join([self.chrom, 
+                       str(self.tx_start), 
+                       str(self.tx_end),
+                       str(self.id),
+                       str(self.fpkm),
+                       strand_int_to_str(self.strand),
+                       str(self.tx_start),
+                       str(self.tx_start),
+                       '0',
+                       str(len(self.exons)),
+                       ','.join(map(str,block_sizes)) + ',',
+                       ','.join(map(str,block_starts)) + ','])
+        return s
--- trunk/assemblyline/assemblyline/lib/transcript_parser.py
+++ trunk/assemblyline/assemblyline/lib/transcript_parser.py
@@ -6,91 +6,48 @@ Created on Nov 2, 2010
 import logging
 import operator
 import collections
-import subprocess
 import os
-import sys
 import math
 
 import gtf
-from base import Exon, NO_STRAND, POS_STRAND, NEG_STRAND, strand_int_to_str, \
-    strand_str_to_int
+from transcript import Exon, Transcript, strand_str_to_int
 
-class Transcript(object):
-    __slots__ = ('chrom', 'start', 'end', 'strand', 'id', 'label', 
-                 'score', 'length', 'exons', 'attrs')
-    def __init__(self):
-        self.chrom = None
-        self.start = -1
-        self.end = -1
-        self.strand = NO_STRAND
-        self.id = None
-        self.label = None
-        self.score = 0
-        self.length = 0
-        self.exons = {}
-        self.attrs = {}
-    def __str__(self):
-        return ("<%s(chrom='%s', start='%d', end='%d', strand='%s', "
-                "id='%s', label='%s', score='%f', length='%d', exons=%s, attrs='%s')>" %
-                (self.__class__.__name__, self.chrom, self.start, self.end, 
-                 strand_int_to_str(self.strand), self.id, self.label, 
-                 self.score, self.length, self.exons, self.attrs))
-    @property
-    def tx_start(self):
-        return self.exons[0].start
-    @property
-    def tx_end(self):
-        return self.exons[-1].end
-    @property
-    def introns(self):
-        return zip([e[1] for e in self.exons[:-1]],
-                   [e[0] for e in self.exons[1:]])
-
-    def to_bed12(self):
-        block_sizes = []
-        block_starts = []
-        for e0, e1 in self.exons:
-            block_starts.append(e0 - self.tx_start)
-            block_sizes.append(e1 - e0)        
-        # write
-        s = '\t'.join([self.chrom, 
-                       str(self.tx_start), 
-                       str(self.tx_end),
-                       str(self.id),
-                       str(self.score),
-                       strand_int_to_str(self.strand),
-                       str(self.tx_start),
-                       str(self.tx_start),
-                       '0',
-                       str(len(self.exons)),
-                       ','.join(map(str,block_sizes)) + ',',
-                       ','.join(map(str,block_starts)) + ','])
-        return s
-
-
-def separate_transcripts(gtf_features, score_attr="FPKM"):
+def separate_transcripts(gtf_features):
     transcripts = collections.defaultdict(lambda: Transcript())
     for feature in gtf_features:
         tx_id = feature.attrs["transcript_id"]
-        seqdata_id = feature.attrs["gene_id"].split(".")[0]
         if feature.feature_type == "exon" and tx_id not in transcripts:            
             logging.error("Feature type 'exon' has no transcript record: %s" % feature)
             assert False
         transcript = transcripts[tx_id]
         if feature.feature_type == "transcript":
-            transcript.id = tx_id
-            transcript.label = seqdata_id
-            # convert from string strand notation ("+", "-", ".") 
-            # to integer (0, 1)            
-            transcript.strand = strand_str_to_int(feature.strand)
             transcript.chrom = feature.seqid
             transcript.start = feature.start
             transcript.end = feature.end
-            score = float(feature.attrs[score_attr])
-            if math.isnan(score):
-                logging.warning("Encountered score='nan' for transcript %s" % (transcript.id))
-                score = 0.0
-            transcript.score = score
+            transcript.exons = {}
+            transcript.attrs = {}
+            # convert from string strand notation ("+", "-", ".") 
+            # to integer (0, 1)            
+            transcript.strand = strand_str_to_int(feature.strand)
+            transcript.id = tx_id
+            transcript.library = feature.attrs["gene_id"].split(".")[0]            
+            transcript.sample = feature.attrs["sample"]
+            transcript.cohort = feature.attrs["cohort"]
+            fpkm = float(feature.attrs["FPKM"])
+            if math.isnan(fpkm):
+                logging.warning("Encountered fpkm='nan' for transcript %s" % (transcript.id))
+                fpkm = 0.0
+            transcript.fpkm = fpkm
+            cov = float(feature.attrs["cov"])
+            if math.isnan(cov):
+                logging.warning("Encountered cov='nan' for transcript %s" % (transcript.id))
+                cov = 0.0
+            transcript.cov = cov
+            frac = float(feature.attrs["frac"])
+            if math.isnan(frac):
+                logging.warning("Encountered frac='nan' for transcript %s" % (transcript.id))
+                frac = 0.0
+            transcript.frac = frac
         elif feature.feature_type == "exon":
             exon_num = int(feature.attrs["exon_number"])
             transcript.exons[exon_num] = Exon(feature.start, feature.end)
@@ -98,9 +55,6 @@ def separate_transcripts(gtf_features, score_attr="FPKM"):
     for transcript in transcripts.itervalues():
         # sort the exons in correct transcript order
         transcript.exons = [transcript.exons[v] for v in sorted(transcript.exons.keys())]
-        # remove the length normalization from scores
-        transcript.length = sum([e.end - e.start for e in transcript.exons])
-        transcript.score = transcript.score * (transcript.length) / 1.0e3        
     # sort transcripts by position
     return sorted(transcripts.values(), key=operator.attrgetter('start'))
 
@@ -136,9 +90,9 @@ def separate_loci(feature_iter):
     if len(window) > 0:
         yield window
 
-def parse_gtf(fileh, attr_defs=None, score_attr="FPKM"):
+def parse_gtf(fileh, attr_defs=None):
     for locus_features in separate_loci(gtf.GTFFeature.parse(fileh, attr_defs)):
-        locus_transcripts = separate_transcripts(locus_features, score_attr)
+        locus_transcripts = separate_transcripts(locus_features)
         yield locus_transcripts
 
 if __name__ == '__main__':
@@ -146,17 +100,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")    
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sort", action="store_true", default=False)
-    parser.add_argument("--score-attr", dest="score_attr", default="FPKM")
     parser.add_argument("filename")
     options = parser.parse_args()
     prefix = os.path.splitext(options.filename)[0]
-    if options.sort:
-        output_file = prefix + ".srt.gtf"
-        sort_gtf(options.filename, output_file)
-    else:
-        output_file = options.filename
-    for locus_transcripts in parse_gtf(open(output_file), attr_defs=None, score_attr=options.score_attr):        
+    output_file = options.filename
+    for locus_transcripts in parse_gtf(open(output_file), attr_defs=None):        
         for t in locus_transcripts:
             print t
         print '------------'
