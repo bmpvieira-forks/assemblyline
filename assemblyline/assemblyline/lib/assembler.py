2e8d815234c0472cea93db22a71c8d50a34d001e
matthew.iyer <matthew.iyer@9703541f-4702-6fd7-b4dd-98584dff58c8>
2011-12-15 06:52:22 +0000

    * revamped path finder (again) to be strictly correct * plans to improve smoothing algorithm

--- assemblyline/assemblyline/lib/assembler.py
+++ assemblyline/assemblyline/lib/assembler.py
@@ -51,47 +51,6 @@ def find_start_and_end_nodes(G, strand):
                  if (d == 0)]
     return start_nodes, end_nodes
 
-def smoothen_graph_leastsq(G):
-    # cannot smooth a graph with just one node
-    if len(G) == 1:
-        return
-    # label nodes
-    cur_id = 0
-    eqns = {}
-    cols = {}
-    y = np.zeros(len(G), dtype=np.float)
-    # assign numeric ids to nodes
-    for n,d in G.nodes_iter(data=True):
-        d['id'] = cur_id
-        y[cur_id] = d[NODE_DENSITY]
-        if G.in_degree(n) > 0:
-            a = np.zeros(len(G), dtype=np.float)
-            a[cur_id] = -1.0
-            eqns[cur_id] = a
-        cols[cur_id] = n
-        cur_id += 1
-    # setup equations for each node
-    for u,v,d in G.edges_iter(data=True):
-        u_id = G.node[u]['id']
-        v_id = G.node[v]['id']
-        out_frac = d[EDGE_OUT_FRAC]
-        #frac = d[EDGE_OUT_FRAC] * d[EDGE_IN_FRAC]
-        eqns[v_id][u_id] = out_frac
-    # build matrix for least squares error
-    # minimization
-    row_ids = sorted(eqns)
-    A = np.vstack(eqns[id] for id in row_ids)
-    A = np.asmatrix(A)
-    I = np.identity(len(G), dtype=np.float)
-    # perform least squares minimization
-    beta = (I - A.T*((A*A.T).I)*A).dot(y)
-    # set new node weights
-    for col_id,n in cols.iteritems():
-        G.node[n][NODE_DENSITY] = beta[0,col_id]
-        del G.node[n]['id']
-    #for n,d in G.nodes_iter(data=True):
-    #    print 'NEW NODE', n, d
-
 def add_dummy_start_end_nodes(G, start_nodes, end_nodes):
     # add 'dummy' tss nodes if necessary
     G.add_node(SOURCE_NODE, attr_dict={NODE_DENSITY: MIN_NODE_DENSITY, 
@@ -147,9 +106,9 @@ def assemble_subgraph(G, strand, fraction_major_path, max_paths):
     # nodes are searched together for the best path
     add_dummy_start_end_nodes(G, start_nodes, end_nodes)
     # compute initial edge density and coverage flow in/out of nodes
-    calculate_edge_fractions(G, NODE_DENSITY)
-    # redistribute node density in order to minimize error in graph
-    smoothen_graph_leastsq(G)
+    # calculate_edge_fractions(G, NODE_DENSITY)
+    # TODO: redistribute node density in order to minimize error in graph
+    # smoothen_graph_leastsq(G)
     # find up to 'max_paths' paths through graph
     path_info_list = []
     for path, density in find_suboptimal_paths(G, SOURCE_NODE, SINK_NODE, 
