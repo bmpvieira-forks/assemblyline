7d96dd407a47201957aaabc8fcac847b083284c8
matthew.iyer <matthew.iyer@9703541f-4702-6fd7-b4dd-98584dff58c8>
2012-11-12 05:50:55 +0000

    more test cases

--- assemblyline/assemblyline/test/test_trim.py
+++ assemblyline/assemblyline/test/test_trim.py
@@ -295,6 +295,37 @@ class TestTrim(unittest.TestCase):
                        Exon(1200,1300), Exon(1300,1500)])
         self.assertTrue(trim_nodes == correct)
 
+    def test_trim_intron_retention(self):
+        transcripts = read_first_locus("trim_intron_retention1.gtf")
+        GG = get_transcript_graphs(transcripts)
+        G,tmap = GG[POS_STRAND]
+        # trim at different thresholds
+        trim_nodes = trim_graph(G, POS_STRAND,
+                                min_trim_length=0, 
+                                trim_utr_fraction=0.0,
+                                trim_intron_fraction=0.01)
+        correct = set()
+        self.assertTrue(trim_nodes == correct)
+        trim_nodes = trim_graph(G, POS_STRAND,
+                                min_trim_length=0, 
+                                trim_utr_fraction=0.0,
+                                trim_intron_fraction=0.11)
+        correct = set([Exon(500,1500)])
+        self.assertTrue(trim_nodes == correct)
+        trim_nodes = trim_graph(G, POS_STRAND,
+                                min_trim_length=0, 
+                                trim_utr_fraction=0.0,
+                                trim_intron_fraction=0.21)
+        correct = set([Exon(500,1500), Exon(2000,9000)])
+        self.assertTrue(trim_nodes == correct)
+        trim_nodes = trim_graph(G, POS_STRAND,
+                                min_trim_length=0, 
+                                trim_utr_fraction=0.0,
+                                trim_intron_fraction=1.0)
+        correct = set([Exon(500,1500), Exon(2000,9000)])
+        self.assertTrue(trim_nodes == correct)
+
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
